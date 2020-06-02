cfd65ae32cf1829e39569942714c999b72ab1997
nakaryo <nakaryo@nakaryonoMacBook-ea.local>
2020-06-02 10:25:16 +0900

    配列への追加記述、インデントの修正、エンターによる終了の実装

--- hometest5.rb
+++ hometest5.rb
@@ -1,30 +1,31 @@
 
 #空の配列を作成
-things  = []
+  things  = []
 
  #質問を繰り返す
-while true do
-puts "→質問：今日は何をしましたか？　入力後エンターを押してください"
-puts "→終了する場合は「以上」と入力してください"
+  while true do
+    puts "→質問：今日は何をしましたか？　入力後、エンターを押してください"
+    puts "→終了する場合はそのままエンター"
 
 # 利用者の入力を受け付ける
   thing = gets.chomp
+
 # 配列に格納
-  things.push(thing)
+  things<<thing
 
 #配列に"以上"が入っているかを調査
-  if things.include?("以上")
-    things.delete("以上")
+  if things.include?("")
+     things.delete("")
 
 # 配列の中の数をカウント
-    num = things.count
+  num = things.count
 #最終的な一覧表示
-  puts "以上と入力されたので、終了処理します!"
+    puts "エンター入力されたので、終了処理します!"
 
-  puts "==========================="
-  puts things
-  puts "本日、あなたは#{num}個のことを行いました！"
-  puts "============================"
+    puts "==========================="
+    puts things
+    puts "本日、あなたは#{num}個のことを行いました！"
+    puts "============================"
     break
   end
 end
