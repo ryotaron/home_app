2da49ba44338207f06da74604538c2595842fa7c
nakaryo <nakaryo@nakaryonoMacBook-ea.local>
2020-06-03 09:58:16 +0900

    インデントの再修正

--- hometest5.rb
+++ hometest5.rb
@@ -1,27 +1,27 @@
-
 #空の配列を作成
-  things  = []
+things  = []
 
- #質問を繰り返す
-  while true do
-    puts "→質問：今日は何をしましたか？　入力後、エンターを押してください"
-    puts "→終了する場合はそのままエンター"
+#質問を繰り返す
+while true do
+  puts "→質問：今日は何をしましたか？  入力後、エンターを押してください"
+  puts "→終了する場合はそのままエンター"
 
-# 利用者の入力を受け付ける
+  #利用者の入力を受け付ける
   thing = gets.chomp
 
-# 配列に格納
-  things<<thing
+  #配列に格納
+  things << thing
 
-#配列に「エンター」＝空入力　の有無を調査
+  #配列に「エンター」＝空入力　の有無を調査
   if things.include?("")
-     things.delete("")
+    things.delete("")
 
-# 配列の中の数をカウント
-  num = things.count
-#最終的な一覧表示
+    #配列の中の数をカウント
+    num = things.count
+
+    #最終的な一覧表示
     puts "エンター入力されたので、終了処理します!"
-
+    puts "《本日の活動まとめ》"
     puts "==========================="
     puts things
     puts "本日、あなたは#{num}個のことを行いました！"
